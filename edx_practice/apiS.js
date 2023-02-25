9146143b6695b81769221bfb554c838cc43fd0bd
andros25 <zsgibbs@outlook.com>
2023-02-24 20:37:03 -0500

    all in a day's coding

--- edx_practice/apiS.js
+++ edx_practice/apiS.js
@@ -1,7 +1,12 @@
+let widthX, heightY;
+
 function init() {
   console.log("0");
   const item1 = document.getElementById("start");
   item1.addEventListener("click", () => makeFrame());
+  item1.addEventListener("click", () => makePlayer());
+  widthX = 500;
+  heightY = 500;
   console.log("INIT");
 }
 
@@ -11,26 +16,54 @@ function makeFrame() {
   let inFrame = document.createElement("iframe");
 
   inFrame.id = "openSSexample";
-  inFrame.width = "500";
-  inFrame.height = "500";
+  inFrame.width = widthX;
+  inFrame.height = heightY;
+  //inFrame.width = "500";
+  //inFrame.height = "500";
   inFrame.src =
     "https://www.openstreetmap.org/export/embed.html?bbox=-77.0855712890625%2C36.972935408083146%2C-76.20254516601564%2C37.415436475081954";
   document.body.appendChild(inFrame);
-
 }
 
+let sourceRepo = [
+  "../siteOne/media/videos/anime.mp4",
+  "../siteOne/media/videos/stol_vet.mp4",
+];
+
 //import video player dynamically
-function makePlayer(){
-let inPlayer = document.createElement("video");
-    inPlayer.src ="../siteOne/media/videos/anime.mp4";
-    inPlayer.controls = true;
-
-let pauseButton = document.createElement("button");
-
-pauseButton.addEventListener("click", ()=>inPlayer.pause());
-
+function makePlayer() {
+  let inPlayer = document.createElement("video");
+  inPlayer.src = sourceRepo[0];
+  inPlayer.id = "beans";
+  inPlayer.controls = true;
+  inPlayer.width = widthX;
+  inPlayer.height = heightY;
   document.body.appendChild(inPlayer);
+
+  beans = document.getElementById("beans");
+
+  //Pause button
+  let pauseButton = document.createElement("button");
+
+  pauseButton.addEventListener("click", () => beans.pause());
   document.body.appendChild(pauseButton);
+
+  //next button
+  let nextPlay = document.createElement("button");
+
+  nextPlay.addEventListener("click", () => turnNext());
+  document.body.appendChild(nextPlay);
+
+  function turnNext() {
+    for (let i = 0; i < sourceRepo.length; i++) {
+      beans.src = sourceRepo[i++];
+
+      if (sourceRepo[i] == 2) {
+        beans.src = sourceRepo[0];
+        break;
+      }
+    }
+  }
 }
 
 window.onload = init();
