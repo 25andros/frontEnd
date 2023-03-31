521ca46280278eb8e667e8c85f40477c1c347a82
andros25 <zsgibbs@outlook.com>
2023-03-31 17:13:44 -0400

    refactoring

--- test/test.js
+++ test/test.js
@@ -1,3 +1,38 @@
+
+
+
+
+let rayInfo = [
+  ["alpha", 5, true],
+  ["bravo", 7, false],
+  ["charlie", 19, true],
+];
+
+function pRay(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[0]);
+  }
+}
+
+
+
+
+let obiN = {
+    title:`russell`,
+    price: 5.99,
+    lang: `eng`,
+    }
+
+
+
+function pObj({title,price,lang}){
+
+    //console.log(`That is good for: ${obj}`);
+    console.log(`That is good for: ${title},${price},${lang}`);
+}
+
+
+
 let customersArray = [
   "Custy Stomer",
   "C. Oostomar",
@@ -9,6 +44,16 @@ let customersArray = [
   "Kasti Yastimeur",
 ];
 
+let frontHalf = ["zack", "75"];
+let backHalf = ["address", "ambi"];
+let whole = [];
+let item = {};
+
+function moveTogether(a, b) {
+  whole = a + b;
+  console.log(a + b);
+}
+
 //Write Your Code here:
 function checkCustomersArray() {
   //customersArray.forEach((el)=>
@@ -19,13 +64,20 @@ function checkCustomersArray() {
     //console.log(`you be good ${cumstomersArray[el]}`);
   }
 }
-
+/*
 function stringPush() {
+
   customersArray.forEach((i) =>
-    //      console.log(`A`),
-    logic()
-  );
+      (
+      console.log(customersArray[i] + " is put at  element " + i)
+          )
+        let objN={},
+        objN[`name`]=customersArray[i],
+        newRay.push(objN),
+      )
+ );
 }
+*/
 
 function logic() {
   console.log(`1 2`);
@@ -37,30 +89,25 @@ customersArray.push();
  */
 }
 
-let newRay=[`orange`];
+let newRay = [`orange`];
 
-function stringPushv2() {
+function stringPush() {
   for (let i = 0; i < customersArray.length; i++) {
     if (typeof customersArray[i] == `string`) {
-      console.log(customersArray[i] + " is put at  element " + i);
-        let objN={};
-        objN[`name`]=customersArray[i];
-        newRay.push(objN);
+      u;
+      objN[`name`] = customersArray[i];
+      newRay.push(objN);
 
+      //newRay.push(`apple`);
+      //newRay.push(customersArray[i]);
+      //customersArray.push(`Apple`);
+      //customersArray.push(customersArray[i]);
 
-        //newRay.push(`apple`);
-        //newRay.push(customersArray[i]);
-        //customersArray.push(`Apple`);
-        //customersArray.push(customersArray[i]);
-
-        //console.log(`a`);
-        //newRay = cuspush(`a`);
+      //console.log(`a`);
+      //newRay = cuspush(`a`);
       continue;
     }
     console.log(customersArray[i] + " has not been put in to element");
-      //customersArray.pop();
+    //customersArray.pop();
   }
 }
-
-//checkCustomersArray();
-//stringPushv2();
